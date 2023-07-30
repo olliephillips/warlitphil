eec59dfaa6ed4ccbed99995c5b4c4b8050bc48e7
Ollie Phillips <ollie@interject.io>
2023-07-30 16:20:37 +0100

    officers using js

---
+++ data/officers.js
@@ -0,0 +1,31 @@
+let officers = [
+    {
+        "name": "Patricia Cockayne",
+        "position": "President"
+    },
+    {
+        "name": "Pat Barlow",
+        "position": "Vice President"
+    },
+    {
+        "name": "Penny Kimber",
+        "position": "Secretary"
+    },
+    {
+        "name": "David Cockayne",
+        "position": "Treasurer"
+    },
+    {
+        "name": "Bill Cooke",
+        "position": "Speaker Secretary"
+    },{
+        "name": "Geraldine Shortland",
+        "position": "IT Secretary"
+    }
+];
+
+let otherMembers = [
+    {
+        "name": "Dave Barlow"
+    }
+];
